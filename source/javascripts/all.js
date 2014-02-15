983b6ad44b34ca066d85ceb444eca66e58114911
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-02-15 11:10:04 -0800

    white list href protocols

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,16 @@
 //= require jquery/dist/jquery
 //= require uri.js/src/URI.js
 $(function() {
+  // parse the `return` href paramater out of the query string
   var href = URI(URI(window.location.search).search(true).return);
-  $("[data-entityId]").each(function() {
-    var this_href = href.clone();
-    this_href.addSearch('entityID', $(this).data('entityid'));
-    $(this).prop('href', this_href);
-  });
+  // whitelist for href protocols 
+  if ( href.protocol() == 'https' ||
+       href.protocol() == 'http' ) {
+    // update page links to IdPs
+    $("[data-entityid]").each(function() {
+      var this_href = href.clone();
+      this_href.addSearch('entityID', $(this).data('entityid'));
+      $(this).prop('href', this_href);
+    });
+  }
 });
