cdbd575d7d9e3421775a0404a3aa4076e872e551
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2014-02-15 12:43:23 -0800

    add a hostname check as well

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,9 +3,9 @@
 $(function() {
   // parse the `return` href paramater out of the query string
   var href = URI(URI(window.location.search).search(true).return);
-  // whitelist for href protocols 
-  if ( href.protocol() == 'https' ||
-       href.protocol() == 'http' ) {
+  // whitelist for href protocols, check that the hostname isn't changing
+  if (( href.protocol() == 'https' || href.protocol() == 'http' )
+     && href.hostname() == window.location.hostname ) {
     // update page links to IdPs
     $("[data-entityid]").each(function() {
       var this_href = href.clone();
